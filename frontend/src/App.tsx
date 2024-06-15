2e28650bbec209160e741f62ae3b8cdcfd2c4b73
FlavorlessQuark <flavorlessquark@gmail.com>
2024-06-15 13:42:44 -0700

    Added context to app and Container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,30 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Home from "./Home";
 import Navbar from "./components/Navbar";
+import Meeting from "./Meeting";
+import { UserProvider } from "./components/UserContext";
+import styled from "styled-components";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-      </Routes>
-      <Navbar />
+        <UserProvider>
+            <Container>
+
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/meeting/:id" element={<Meeting/>} />
+            </Routes>
+            <Navbar />
+            </Container>
+        </UserProvider>
     </Router>
   );
 };
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+`
 
 export default App;
